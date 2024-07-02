92702f9972b2d48be0b24cae7583c8b090e4456a
Divyansh <divyanshracvik2209@gmail.com>
2024-07-02 22:43:33 +0530

    final code

---
+++ index.js
@@ -0,0 +1,160 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const cors = require('cors')
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const crypto = require("crypto");
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+
+
+const { createProduct } = require('./controller/Product');
+const productsRouter = require('./routes/Products');
+const categoriesRouter = require('./routes/Categories');
+const brandsRouter = require('./routes/Brands');
+const usersRouter = require('./routes/Users');
+const authRouter = require('./routes/Auth');
+const cartRouter = require('./routes/Cart');
+const ordersRouter = require('./routes/Order');
+const { User } = require('./model/User');
+const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
+const path = require('path');
+
+//jwt options:
+var opts = {}
+opts.jwtFromRequest = cookieExtractor;
+opts.secretOrKey = process.env.JWT_SECRET_KEY;
+
+//middlewares
+server.use(express.static(path.resolve(__dirname, 'build')));
+server.use(cookieParser())
+server.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false, // don't save session if unmodified
+        saveUninitialized: false, // don't create session until something stored
+    })
+);
+server.use(passport.authenticate('session'));
+passport.use('local', new LocalStrategy(
+    { usernameField: 'email' },
+    async function (email, password, done) {
+        try {
+            const user = await User.findOne(
+                { email: email },
+            ).exec();
+
+            if (!user) {
+                done(null, false, { message: 'invalid credentials' });
+            }
+
+            crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', async function (err, hashedPassword) {
+                // TODO: this is just temporary, we will use strong password auth
+                console.log({ user })
+                if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+                    // TODO: We will make addresses independent of login
+                    done(null, false, { message: 'invalid credentials' });
+                }
+                const token = jwt.sign(sanitizeUser(user), process.env.JWT_SECRET_KEY);
+                done(null, { id: user.id, role: user.role , token });
+            })
+        } catch (err) {
+            done(err);
+        }
+    }
+));
+
+
+passport.use(
+    'jwt',
+    new JwtStrategy(opts, async function (jwt_payload, done) {
+        console.log({ jwt_payload });
+        try {
+            const user = await User.findOne({ id: jwt_payload.sub });
+            if (user) {
+                return done(null, sanitizeUser(user)); // this calls serializer
+            } else {
+                return done(null, false);
+            }
+        } catch (err) {
+            return done(err, false);
+        }
+    })
+);
+
+passport.serializeUser(function (user, cb) {
+    console.log('serialize', user);
+    process.nextTick(function () {
+        return cb(null, { id: user.id, role: user.role });
+    });
+});
+
+// this changes session variable req.user when called from authorized request
+passport.deserializeUser(function (user, cb) {
+    console.log('de-serialize', user);
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+//payment
+
+// const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
+
+// const calculateOrderAmount = (items) => {
+//     return 1400;
+// };
+
+// server.post("/create-payment-intent", async (req, res) => {
+//     console.log("REQUEST:" , req.body);
+//     const { items } = req.body;
+
+//     // Create a PaymentIntent with the order amount and currency
+//     const paymentIntent = await stripe.paymentIntents.create({
+//         amount: calculateOrderAmount(items),
+//         currency: "inr",
+//         // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+//         automatic_payment_methods: {
+//             enabled: true,
+//         },
+//     });
+
+//     res.send({
+//         clientSecret: paymentIntent.client_secret,
+//     });
+// });
+
+
+
+server.use(cors({
+    exposedHeaders: ['X-Total-Count']
+}))
+server.use(express.json()); // to parse req.body
+server.use('/products', isAuth(), productsRouter.router);// we can also use JWT token for client-only auth
+server.use('/categories', isAuth(), categoriesRouter.router);
+server.use('/brands', isAuth(), brandsRouter.router);
+server.use('/users', isAuth(), usersRouter.router);
+server.use('/auth', authRouter.router);
+server.use('/cart', isAuth(), cartRouter.router);
+server.use('/orders', isAuth(), ordersRouter.router);
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('database connected')
+}
+
+server.get('/', (req, res) => {
+    res.json({ status: 'success' })
+})
+
+
+server.listen(process.env.PORT , () => {
+    console.log('server started')
+})
